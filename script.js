af8faf794a350b0c0f33a9f09257b3c5bf57b632
naisannovel <naisannovel@gmail.com>
2021-02-07 16:21:50 +0600

    update

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(response => response.json())
-.then(data => defaultCategories(data))
-
-
-function defaultCategories(data){
-    let allCategories = data.categories
-    allCategories.forEach(element => {
-        let mainBox = document.getElementById('list-item')
-        let newBox = document.createElement('div')
-        newBox.className = 'cate'
-        let list = `
-        <img src="${element.strCategoryThumb}">
-        <h5>${element.strCategory}</h5>
-        `
-        newBox.innerHTML = list;
-        mainBox.appendChild(newBox)
-    });
-}
-
 document.getElementById('search-btn').addEventListener('click',function(){
     let searchValue = document.getElementById('search-inp').value;
     
     if(searchValue.length != 0){
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
     .then(response => response.json())
-    .then(data => display(data))
+    .then(data => {
+        console.log(data);
+        display(data);
+    })
     }else{
         alert('Search box are empty')
     }
@@ -35,18 +18,19 @@ function display(data){
     let cat = data.meals
     if(cat != null){
     cat.forEach(element => {
-        document.getElementById('list-item').style.display = 'none'
         let mainBox = document.getElementById('search-list-item')
         let newBox = document.createElement('div');
         let list = `
-        <div class="cate" onclick="functionName('${element.strMeal}')">
+        <div class="cate" onclick="functionName('${element.strMeal}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
         <img src="${element.strMealThumb}">
         <h5>${element.strMeal}</h5>
         </div>
         `
         newBox.innerHTML = list;
-        mainBox.appendChild(newBox)
-    })
+        mainBox.appendChild(newBox);
+
+    });
+
 }else{
     alert('no food avileable')
 }
@@ -61,9 +45,20 @@ function functionName(nam){
 }
 
 function displayB(data){
-    let element = data.meals
-    console.log(element[0].strMeal);
-    console.log(element[0].strMealThumb);
-    console.log(element[0].strIngredient1);
+    let element = data.meals;
+    let nov = element[0];
+        console.log(element[0]);
 
+    
+    for (let i = 1; i <= 20; i++) {
+        const element = i;
+        let x = 'strIngredient' + i;
+        
+        console.log(nov[x]);
+        
+        
+    }
+   
 }
+
+
