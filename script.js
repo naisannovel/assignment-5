fb8a634fe939d203f1d5365c42c46a7f21d483c7
naisannovel <naisannovel@gmail.com>
2021-02-07 17:16:31 +0600

    update

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-document.getElementById('search-btn').addEventListener('click',function(){
+
+// search meal
+
+document.getElementById('search-btn').addEventListener('click', function () {
     let searchValue = document.getElementById('search-inp').value;
-    
-    if(searchValue.length != 0){
+
+    if (searchValue.length != 0) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        display(data);
-    })
-    }else{
+            .then(response => response.json())
+            .then(data => display(data)) //call another function. 
+    } else {
         alert('Search box are empty')
     }
     document.getElementById('search-inp').value = '';
 })
 
-function display(data){
-    let cat = data.meals
-    if(cat != null){
-    cat.forEach(element => {
-        let mainBox = document.getElementById('search-list-item')
-        let newBox = document.createElement('div');
-        let list = `
+function display(data) {
+    let allMealElements = data.meals
+    console.log(allMealElements);
+    if (allMealElements != null) {
+        allMealElements.forEach(element => {
+            let mainUiBox = document.getElementById('search-list-item')
+            let newBox = document.createElement('div');
+            let list = `
         <div class="cate" onclick="functionName('${element.strMeal}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
         <img src="${element.strMealThumb}">
         <h5>${element.strMeal}</h5>
         </div>
         `
-        newBox.innerHTML = list;
-        mainBox.appendChild(newBox);
+            newBox.innerHTML = list;
+            mainUiBox.appendChild(newBox);
 
-    });
+        });
 
-}else{
-    alert('no food avileable')
+    } else {
+        alert('no food avileable')
+    }
+
 }
-   
-}
 
 
-function functionName(nam){
+function functionName(nam) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${nam}`)
-    .then(response => response.json())
-    .then(data => displayB(data))
+        .then(response => response.json())
+        .then(data => displayB(data))
 }
 
-function displayB(data){
+function displayB(data) {
     let element = data.meals;
     let nov = element[0];
-        console.log(element[0]);
+    console.log(element[0]);
 
-    
+
     for (let i = 1; i <= 20; i++) {
         const element = i;
         let x = 'strIngredient' + i;
-        
-        console.log(nov[x]);
-        
-        
+
+        if (nov[x].length != 0) {
+            console.log(nov[x]);
+        }
+
+
     }
-   
+
 }
-
-
